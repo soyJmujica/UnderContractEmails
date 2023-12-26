1c3ec90d4daa70dc486af909c963e7645948222a
Jose Mujica <master1495@gmail.com>
2023-12-25 20:23:27 -0400

    poetry added to build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 
 set -o errexit
 
-#poetry install
-#pip install -r requirements.txt
+poetry install
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
