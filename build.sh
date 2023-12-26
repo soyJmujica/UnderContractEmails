8e6519dbefd561a93ca724ce90a9754e13366c4b
Jose Mujica <master1495@gmail.com>
2023-12-26 13:38:41 -0400

    settings.py & build.sh updated

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ set -o errexit
 poetry install
 poetry add gunicorn
 poetry add django
+poetry add dj-database-url psycopg2-binary
+poetry add 'whitenoise[brotli]'
 
 python manage.py collectstatic --no-input
 python manage.py migrate
