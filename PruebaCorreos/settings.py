34a633181009063bfa530b31b1ff4d352485bf68
Jose Mujica <master1495@gmail.com>
2023-12-27 16:16:44 -0400

    settings updated

--- PruebaCorreos/settings.py
+++ PruebaCorreos/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "djmoney", "naomi",'Correos.apps.RenderConfig',
+    "djmoney", "naomi",'Correos.apps.CorreosConfig',
     'Team.apps.RenderConfig',
     #Mias
     "Correos",
